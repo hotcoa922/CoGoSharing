b6aa977c5eedd5f2b038640647aa38ea1bb3208d
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2024-07-12 18:12:13 +0900

    feat: start project

---
+++ src/main/java/com/example/cogosharing/CoGoSharingApplication.java
@@ -0,0 +1,13 @@
+package com.example.cogosharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoGoSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoGoSharingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cogosharing/CoGoSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cogosharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoGoSharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
