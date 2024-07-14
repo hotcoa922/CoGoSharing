f7ddd7f548d5498397045162e70ec973bd0e3750
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2024-07-14 13:48:59 +0900

    feat: add couple and group entities

--- src/main/java/com/example/cogosharing/entity/MemberShip.java
+++ src/main/java/com/example/cogosharing/entity/MemberShip.java
@@ -12,25 +12,34 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Table(name = "member_ship")
 public class MemberShip extends BaseTimeEntity {
-    //맴버 타입에 대한 정보를 가짐
-
 
     @Id
-    @Column(name = "couple_id", unique = true, nullable = false)
+    @Column(name = "member_ship_id", unique = true, nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "membership_type", nullable = false)
+    @Column(name = "member_ship_type", nullable = false)
     private MemberShipType memberShipType;
 
-    @Column(name = "memberShip_id", nullable = false)
+    @Column(name = "member_ship_id", nullable = false)
     private Long memberShipId;  //커플 혹은 그룹 id
 
     //User - Membership = 1:N
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    //Couple : MemberShip = 1 : N
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "couple_id")
+    private Couple couple;
+
+
+    //Couple : MemberShip = 1 : N
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id")
+    private Group group;
 
 }
 
---
+++ src/main/java/com/example/cogosharing/entity/Participant.java
@@ -0,0 +1,2 @@
+package com.example.cogosharing.entity;public class Participant {
+}
