eceb098c14229ef0951ced2e68d20e0fd4e2b4d9
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2024-07-17 17:00:53 +0900

    feat: implement services for user registration, login, and logout

--- src/main/java/com/example/cogosharing/service/UserService.java
+++ src/main/java/com/example/cogosharing/service/UserService.java
@@ -1,9 +1,13 @@
 package com.example.cogosharing.service;
 
+import com.example.cogosharing.dto.user.LoginRequestDto;
 import com.example.cogosharing.dto.user.RegisterRequestDto;
 import com.example.cogosharing.entity.User;
+import com.example.cogosharing.jwt.JwtTokenProvider;
 import com.example.cogosharing.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +16,13 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtTokenProvider jwtTokenProvider) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
 
@@ -28,6 +34,19 @@ public class UserService {
                 .build();
 
         userRepository.save(user);
+    }
 
+    public String loginUser(LoginRequestDto loginRequestDto) {
+        User user = userRepository.findByEmail(loginRequestDto.getEmail());
+        if (user == null) {
+            throw new UsernameNotFoundException(loginRequestDto.getEmail());
+        }
+
+
+        if (!passwordEncoder.matches(loginRequestDto.getPassword(), user.getPassword())) {
+            throw new BadCredentialsException("Invalid credentials");
+        }
+
+        return jwtTokenProvider.createToken(user.getEmail(), user.getUserName());
     }
 }
