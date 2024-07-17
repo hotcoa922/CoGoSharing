cbc07ba4a228d021bff175cb43fbe85d827843fe
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2024-07-17 15:25:05 +0900

    feat: add findByUserName

--- src/main/java/com/example/cogosharing/repository/UserRepository.java
+++ src/main/java/com/example/cogosharing/repository/UserRepository.java
@@ -4,8 +4,11 @@ import com.example.cogosharing.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    User findByUserName(String username);
 }
