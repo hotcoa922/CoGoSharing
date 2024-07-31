849083ab2ed29508452abdd989f4a1d395cfda00
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2024-07-31 16:27:09 +0900

    refactor: separate services into interfaces

--- src/main/java/com/example/cogosharing/service/CoupleService.java
+++ src/main/java/com/example/cogosharing/service/CoupleService.java
@@ -1,67 +1,12 @@
 package com.example.cogosharing.service;
 
-
 import com.example.cogosharing.dto.membership.MemberShipRequestDto;
 import com.example.cogosharing.dto.membership.RegisterCoupleRequestDto;
 import com.example.cogosharing.entity.Couple;
 import com.example.cogosharing.entity.MemberShip;
-import com.example.cogosharing.entity.User;
-import com.example.cogosharing.enums.MemberShipType;
-import com.example.cogosharing.repository.CoupleRepository;
-import com.example.cogosharing.repository.MemberShipRepository;
-import com.example.cogosharing.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-public class CoupleService {
-
-    private final UserRepository userRepository;
-    private final CoupleRepository coupleRepository;
-    private final MemberShipRepository memberShipRepository;
-
-    @Autowired
-    public CoupleService(CoupleRepository coupleRepository, UserRepository userRepository, MemberShipRepository memberShipRepository ) {
-        this.coupleRepository = coupleRepository;
-        this.userRepository = userRepository;
-        this.memberShipRepository = memberShipRepository;
-    }
-
-    // 커플 생성
-    @Transactional
-    public Couple createCouple(RegisterCoupleRequestDto registerCoupleRequestDto) {
-        Couple couple = Couple.builder()
-                .coupleName(registerCoupleRequestDto.getCoupleName())
-                .build();
-        return coupleRepository.save(couple);
-    }
-
-    // 커플에 가입
-    @Transactional
-    public MemberShip joinCouple(MemberShipRequestDto memberShipRequestDto) {
-        User user = userRepository.findById(memberShipRequestDto.getUserId())
-                .orElseThrow(() -> new RuntimeException("사용자 없음"));
-        Couple couple = coupleRepository.findById(memberShipRequestDto.getCoupleId())
-                .orElseThrow(() -> new RuntimeException("커플 없음"));
-
-        MemberShip memberShip = MemberShip.builder()
-                .user(user)
-                .couple(couple)
-                .memberShipType(MemberShipType.Cop)
-                .build();
-        return memberShipRepository.save(memberShip);
-    }
-
-    // 커플 탈퇴
-    @Transactional
-    public void leaveCouple(MemberShipRequestDto memberShipRequestDto) {
-        User user = userRepository.findById(memberShipRequestDto.getUserId())
-                .orElseThrow(() -> new RuntimeException("사용자 없음"));
-        Couple couple = coupleRepository.findById(memberShipRequestDto.getCoupleId())
-                .orElseThrow(() -> new RuntimeException("커플 없음"));
-
-        memberShipRepository.deleteByUserAndCouple(user, couple);
-    }
-
+public interface CoupleService {
+    Couple createCouple(RegisterCoupleRequestDto registerCoupleRequestDto);
+    MemberShip joinCouple(MemberShipRequestDto memberShipRequestDto);
+    void leaveCouple(MemberShipRequestDto memberShipRequestDto);
 }
--- src/main/java/com/example/cogosharing/service/GroupService.java
+++ src/main/java/com/example/cogosharing/service/GroupService.java
@@ -4,65 +4,10 @@ import com.example.cogosharing.dto.membership.MemberShipRequestDto;
 import com.example.cogosharing.dto.membership.RegisterGroupRequestDto;
 import com.example.cogosharing.entity.Group;
 import com.example.cogosharing.entity.MemberShip;
-import com.example.cogosharing.entity.User;
-import com.example.cogosharing.enums.MemberShipType;
-import com.example.cogosharing.repository.GroupRepository;
-import com.example.cogosharing.repository.MemberShipRepository;
-import com.example.cogosharing.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-public class GroupService {
-
-    private final UserRepository userRepository;
-    private final GroupRepository groupRepository;
-    private final MemberShipRepository memberShipRepository;
-    
-
-    @Autowired
-    public GroupService(GroupRepository groupRepository, UserRepository userRepository, MemberShipRepository memberShipRepository) {
-        this.groupRepository = groupRepository;
-        this.userRepository = userRepository;
-        this.memberShipRepository = memberShipRepository;
-    }
-
-
-    //그룹 생성
-    @Transactional
-    public Group createGroup(RegisterGroupRequestDto registerGroupRequestDto) {
-        Group group = Group.builder()
-                .groupName(registerGroupRequestDto.getGroupName())
-                .build();
-        return groupRepository.save(group);
-    }
-
-    //그룹에 가입
-    @Transactional
-    public MemberShip joinGroup(MemberShipRequestDto memberShipRequestDto) {
-        User user = userRepository.findById(memberShipRequestDto.getUserId())
-                .orElseThrow(() -> new RuntimeException("사용자 없음"));
-        Group group = groupRepository.findById(memberShipRequestDto.getGroupId())
-                .orElseThrow(() -> new RuntimeException("그룹 없음"));
-
-        MemberShip memberShip = MemberShip.builder()
-                .user(user)
-                .group(group)
-                .memberShipType(MemberShipType.Gop)
-                .build();
-        return memberShipRepository.save(memberShip);
-    }
-
-    //그룹 탈퇴
-    @Transactional
-    public void leaveGroup(MemberShipRequestDto memberShipRequestDto) {
-        User user = userRepository.findById(memberShipRequestDto.getUserId())
-                .orElseThrow(() -> new RuntimeException("사용자 없음"));
-        Group group = groupRepository.findById(memberShipRequestDto.getGroupId())
-                .orElseThrow(() -> new RuntimeException("그룹 없음"));
-
-        memberShipRepository.deleteByUserAndGroup(user, group);
-    }
+public interface GroupService {
+    Group createGroup(RegisterGroupRequestDto registerGroupRequestDto);
+    MemberShip joinGroup(MemberShipRequestDto memberShipRequestDto);
+    void leaveGroup(MemberShipRequestDto memberShipRequestDto);
 
 }
